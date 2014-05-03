a7b0d2a06272e3e086e87efe59148b9785376288
Carlos Hinostroza <chinostroza@gmail.com>
2014-05-03 00:01:50 -0400

    new handler

--- controlprint/designaddhandler.py
+++ controlprint/designaddhandler.py
@@ -21,7 +21,8 @@ class DesignAddHandler(BaseHandler):
 		design.title=self.get_argument("title","")
 		design.avatar=self.get_argument("avatar","")
 		design.user=self.get_argument("user","")
-		design.id=self.get_argument("id","")
+		design.sku=self.get_argument("sku","")
+		design.identifier=self.get_argument("identifier","")
 		design.description=self.get_argument("description","")
 		design.Save()
 
