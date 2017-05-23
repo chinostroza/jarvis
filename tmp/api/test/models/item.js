714ae1a07175838e8559d16da4762e3909488095
Carlos Hinostroza <chinostroza@gmail.com>
2017-05-23 10:49:59 -0400

    clean

--- tmp/api/test/models/item.js
+++
@@ -1,9 +0,0 @@
-exports = module.exports = function(app, mongoose) {
-
-	var itemSchema = new mongoose.Schema({
-		id: { type:Number},order_id: { type:Number},code: { type:String},title: { type:String},quantity: { type:Number},status: { type:Number},created_ad: { type:Date},updated_ad: { type:Date}
-	});
-
-	mongoose.model('Item', itemSchema);
-
-};
