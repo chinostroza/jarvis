4f693fe8d310719686e0774c526ec3fbb907920a
Carlos Hinostroza <chinostroza@gmail.com>
2017-05-23 11:58:12 -0400

    add android data RestApi.java template

---
+++ templates/android/data/net/RestApi.java
@@ -0,0 +1,38 @@
+package {{ app.package }}.data.source.remote.net;
+
+{% for object in objects %}
+import {{ app.package }}.data.source.remote.net.wrapper.{{object.name|capitalize}}Wrapper;
+import {{ app.package }}.domain.model.{{object.name|capitalize}};
+{% endfor %}
+
+import java.util.List;
+
+import retrofit2.Response;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import io.reactivex.Observable;
+
+public interface RestApi {
+
+    String URL_BASE = "{{ api.url_base }}";
+    int API_VERSION = {{ api.version }};
+    String VERSION_HEADER = "{{ api.version_header }}" + API_VERSION;
+
+    {% for resource in api.resources %}
+    @{{ resource.http_method }}("{{ resource.url }}")
+    Observable<Response<{{ resource.response }}>> {{ resource.method }}(
+        {% for param in resource.params %}
+        {% if loop.last %}
+        {% autoescape false %}{{ param.decorator }}{% endautoescape %} {{ param.type }} {{ param.name }}
+        {% else %}
+        {% autoescape false %}{{ param.decorator }}{% endautoescape %} {{ param.type }} {{ param.name }},
+        {% endif %}
+        {% endfor %});
+
+    {% endfor %}
+}
